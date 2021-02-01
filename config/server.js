ad59dcb6baa4484b2d7317242cbebb197b3c277e
Derric <realbenneh@gmail.com>
2021-02-01 13:58:52 +0000

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0779ef90dd2c9524900f9d5249ad943'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f69c59eb-802e-49fa-bf77-941c4b2a4130'
+};
